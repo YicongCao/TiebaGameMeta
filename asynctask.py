f9df6dd2eb695be7312b934da17627707fee4e8a
yicongcao <yicongcao@gmail.com>
2019-03-16 12:03:54 +0800

    update async task logic adding skip done ones

--- asynctask.py
+++ asynctask.py
@@ -20,12 +20,17 @@ def crawl_tieba_info(game_tieba, basic_type_name):
         if not os.path.isdir(folder):
             os.makedirs(folder)
     except OSError:
-        LOGGER.ERROR('err making dir ' + folder)
+        LOGGER.error('err making dir ' + folder)
         return
     try:
+        filename = folder + "/" + game_tieba['gamename'] + ".html"
+        # 跳过已经下载的
+        if os.path.isfile(filename):
+            LOGGER.info('file exists: '+filename)
+            return
         html = requests.get(game_tieba['link'], stream=True)
-        with open(folder + "/" + game_tieba['gamename'] + ".html", 'wb') as f:
+        with open(filename, 'wb') as f:
             f.write(html.content)
-        # LOGGER.INFO('done: '+game_tieba['gamename'])
+        # LOGGER.info('done: '+game_tieba['gamename'])
     except Exception as exc:
-        LOGGER.ERROR('err in '+game_tieba['gamename'])
+        LOGGER.error('err in ' + game_tieba['gamename'])
